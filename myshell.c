348ba21bb416e57054a3f207a5108e22812740e2
CoderXt <724435851@qq.com>
2022-09-22 07:33:18 -0700

    simple bash by myself

---
+++ myshell.c
@@ -0,0 +1,62 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<string.h>
+#include<sys/wait.h>
+#include<sys/types.h>
+
+#define NUM 1024
+#define SIZE 32
+#define SEP " "
+char cmd_line[NUM];
+char* g_argv[SIZE];
+
+int main()
+{
+    while(1){
+        //打印提示信息
+        printf("[root@localhost myshell]# ");
+        fflush(stdout);
+        memset(cmd_line, '\0', sizeof cmd_line);
+        //读取命令行字符串
+        if(fgets(cmd_line, sizeof cmd_line, stdin) == NULL){
+            continue;
+        }
+        //解析命令行字符串
+        cmd_line[strlen(cmd_line)-1] = '\0';
+        g_argv[0] = strtok(cmd_line, SEP);
+        int index = 1;
+        
+        //让ls命令带配色
+        if(strcmp(g_argv[0], "ls") == 0){
+            g_argv[index++] = (char*)"--color=auto";
+        }
+
+        while(g_argv[index++] = strtok(NULL, SEP));
+        
+        //int i = 0;
+        //for(i = 0; g_argv[i]; i++){
+        //    printf("%d->%s\n",i, g_argv[i]);
+        //}
+        //处理内置命令
+        if(strcmp(g_argv[0], "cd") == 0){
+            if(g_argv[1] != NULL){
+                chdir(g_argv[1]);
+            }
+            continue;
+        }
+        //执行命令
+        pid_t id = fork();
+        if(id == 0){
+            printf("以下功能为子进程执行\n");
+            execvp(g_argv[0],g_argv);
+            exit(1);
+        }
+        int status = 0;
+        pid_t ret = waitpid(id, &status, 0);
+        if(ret > 0){
+            printf("exit code: %d\n %d\n",WEXITSTATUS(status), WIFEXITED(status));
+        }
+    }
+    return 0;
+}
